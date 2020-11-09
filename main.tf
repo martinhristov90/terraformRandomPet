0b77e3989c042c7dcd6fe79bda726c5f0b59c8e4
Martin Hristov <mhristov@hashicorp.com>
2020-11-09 15:57:01 +0200

    removing label for provisioner

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "random_pet" "animal" {
   separator = " "
 
   # Printing the value of the randomly generated pet.
-  provisioner "local-exec" "printing" {
+  provisioner "local-exec" {
     command = "echo Here is a random pet name : ${random_pet.animal.0.id}"
   }
 }
